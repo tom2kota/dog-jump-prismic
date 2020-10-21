95d9ad3290a5a6a47b5bff7e5ff7337823cb6663
Web Dev <tom2kota@gmail.com>
2020-10-22 00:36:43 +0300

    Refactor DefaultLayout

--- src/components/layout/DefaultLayout.jsx
+++ src/components/layout/DefaultLayout.jsx
@@ -2,13 +2,10 @@ import React from "react"
 import {Footer} from "./Footer";
 import {Header} from "./Header";
 
-export const DefaultLayout = ({wrapperClass, menuDoc, children}) => {
-
-    return (
-        <div className={wrapperClass}>
-            <Header menuDoc={menuDoc}/>
-            {children}
-            <Footer/>
-        </div>
-    )
-}
+export const DefaultLayout = ({wrapperClass, menuDoc, children}) => (
+    <div className={wrapperClass}>
+        <Header menuDoc={menuDoc}/>
+        {children}
+        <Footer/>
+    </div>
+)
